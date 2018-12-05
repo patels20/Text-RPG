eb1cc2c89019e3987410313595c228217e820bdb
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 18:10:02 -0500

    Update level.cpp

--- level.cpp
+++ level.cpp
@@ -4,30 +4,33 @@
 #include <stdlib.h>
 #include <random>
 
+using namespace std;
+
+
 int randomFight = (rand() %50) +20;
 int userfightInt = 0;
-void level1(string userInt, player& player, monster& mon)
+void level1(char userInt, player player1, monster mon)
 {
     cout << "You're walking in the woods.  There's no one around and your phone is dead.  Out of the corner of your eye, you spot him:" << endl;
-    cout << "SHIA LEBEOUF" << endl:
+    cout << "SHIA LEBEOUF" << endl;
     cout << "He looks into your eyes, you can see theres blood on your face.  My God, theres blood everywhere!" << endl;
-    cout << "Do you run or fight? << endl;
+    cout << "Do you run or fight? "<< endl;
     cout << "------------------------------ " << endl;
-    cout << "\t\t\tWelcome To Level 1" << endl:
+    cout << "\t\t\tWelcome To Level 1" << endl;
     cout << "------------------------------" << endl;
     cout << "Which direction would you want to take?" << endl;
     cin >> userInt;
     system("cls");
     switch(userInt)
     {
-        case "Right":
-        case "right":
-            cout << "You've initiated to fight " << monster.getName() << endl;
+        case 'R':
+        case 'r':
+            // cout << "You've initiated to fight " << mon.getName() << endl;
             do
             {
                 cout << "------------------------------ \n" << endl;
-                cout <<" Your Health : " << player.health << endl;
-                cout << "Monster Health : " << monster.health <<  "\n" << endl;
+                cout <<" Your Health : " << player1.health << endl;
+                cout << "Monster Health : " << mon.health <<  "\n" << endl;
                 cout << "------------------------------ \n" << endl;
 
                 cout << "\t\tYou're Turn" << endl;
@@ -39,32 +42,32 @@ void level1(string userInt, player& player, monster& mon)
                 switch(userfightInt)
                 {
                     case 1:
-                        monster.sethealth((monster.health)-player.attack)
+                        mon.setHealth((mon.health)-player1.attack);
                         break;
                     case 2:
                         cout << "You've Dodge the attack" << endl;
                     case 3:
-                        if(player.health + 10 >= 100)
+                        if((player1.health + 10) >= 100)
                         {
                             cout << "You'v regained some health " << endl;
-                            player.setHealth(100);
-                            cout << "You're Current health is : " << player.getHealth() << endl;
+                            player1.setHealth(100);
+                       //     cout << "You're Current health is : " << player1.getHealth() << endl;
                         }
                         else
                         {
                             cout << "You've regained some health " << endl;
-                            player.setHealth(player.health+10);
-                            cout << "You're Current health is : " << player.getHealth() << endl;
+                            player1.setHealth(player1.health+10);
+                    //        cout << "You're Current health is : " << player1.getHealth() << endl;
                         }
 
                 }
 
             }
-            while(monster.health < 0; || player.health < 0)
-        case "Left":
-        case "left":
-            cout << "You've Encountered " << monster.getName() << endl;
-            cout << ""
+            while((mon.health < 0) || (player1.health < 0));
+        case 'L':
+        case 'l':
+      //      cout << "You've Encountered " << mon.getName() << endl;
+            cout << "" << endl;
 
 
 
@@ -72,8 +75,6 @@ void level1(string userInt, player& player, monster& mon)
 
 
     }
-
-
 
 
 }
