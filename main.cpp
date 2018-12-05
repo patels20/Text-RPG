fc404b70fa72720511b1c3d94e0b762d531d9b1f
akshar008 <36671757+akshar008@users.noreply.github.com>
2018-12-05 17:21:53 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 
 int main()
 {
-
+    cout << "Please pick one of the characters below:";
+    cout << "\n\n(1)\nWizard\nWeapon: Staff\n";
+    cout << "\n\n(2)\nArcher\nWeapon: Bow and Arrow\n";
+    cout << "\n\n(3)\nWarrior\nWeapon: Shield and Sword\n";
+    return 0;
 
 
 
