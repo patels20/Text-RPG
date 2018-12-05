eb0cde2d215408d67eccc11697fc6fe32518ad7c
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 14:40:39 -0500

    Create wizard.h

---
+++ wizard.h
@@ -0,0 +1,26 @@
+#ifndef WIZARD_H
+#define WIZARD_H
+
+#include <player.h>
+
+
+class wizard : public player
+{
+    private:
+        string spellBook[2];
+        int mana;
+    public:
+        int setMana(man)
+        {
+            mana = man;
+        }
+        
+        
+        
+        
+        
+        
+        
+};
+
+#endif // WIZARD_H
