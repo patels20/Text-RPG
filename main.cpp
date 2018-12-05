526284b4f63b55468a959adce238131d3cb3a8c6
akshar008 <36671757+akshar008@users.noreply.github.com>
2018-12-05 17:23:31 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,15 @@ int main()
     cout << "\n\n(2)\nArcher\nWeapon: Bow and Arrow\n";
     cout << "\n\n(3)\nWarrior\nWeapon: Shield and Sword\n";
     return 0;
+    if(person > 3)
+		{
+			system("cls");
+			cout << "You must enter an integer between 1 and 3!!!!\n\n";
+			system("pause");
+			system("cls");
+		}
 
-
+    
+    
 
 }
