5cffb6c463505508d2f7113ef5d74d2eb61c8b11
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 16:20:24 -0500

    Create monster.h

---
+++ monster.h
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include "player.h"
+#include "level.h"
+#ifndef MONSTER.H
+
+
+using namespace std;
+
+class monster
+{
+    private:
+        string name;
+        int health;
+        int attack;
+        int level;
+    public:
+        monster()
+        {
+            //Default Constructor
+        }
+        monster(string n, int h, int a, int l)
+        {
+            //Primary Constructor
+            setName(n);
+            setHealth(h);
+            setAttack(a);
+            setLevel(l);
+        }
+        ~monster()
+        {
+            //Default Destructor
+        }
+        string setName(string na)
+        {
+            name = na;
+        }
+        int setHealth(int hea)
+        {
+            health = hea;
+        }
+        int setAttack(int att)
+        {
+            attack = att;
+        }
+        int setLevel(int levl)
+        {
+            level =levl;
+        }
+
+
+
+};
+
+#endif // MONSTER
