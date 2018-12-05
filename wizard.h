292d6e2121bf544f8c99f9228c2897963900b1d8
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 16:20:01 -0500

    Update wizard.h

--- wizard.h
+++ wizard.h
@@ -1,26 +1,33 @@
 #ifndef WIZARD_H
 #define WIZARD_H
 
-#include <player.h>
+#include "player.h"
 
 
 class wizard : public player
 {
     private:
-        string spellBook[2];
-        int mana;
+        string spellBook[2]
     public:
-        int setMana(man)
+        wizard()
         {
-            mana = man;
+            //Default Constructor
         }
-        
-        
-        
-        
-        
-        
-        
+        wizard(string n, int h,int a, int e, int c, int en)
+       {
+           //Primary constructor
+           setName(n);
+           setHealth(h);
+           setAttack(a);
+           setExperience(e);
+           setCash(c);
+           setEnergy(en);
+        }
+        ~wizard()
+        {
+            //Default Destructor
+        }
+
 };
 
 #endif // WIZARD_H
