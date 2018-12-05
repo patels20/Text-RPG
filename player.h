ce1e83dc517943bdc40053709b7aefa60677cda8
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 14:40:04 -0500

    Create player.h

---
+++ player.h
@@ -0,0 +1,54 @@
+#include <string>
+#include <iostream>
+#include <random>
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#define INVENT_SIZE 10
+
+class player
+{
+    private:
+        string name;
+        int health;
+        int magicLevel;
+        int speed;
+        int attack;
+        int experience;
+        int cash;
+        string inventory[INVENT_SIZE];
+    public:
+        string setName(nameVar)
+        {
+            name = nameVar;
+        }
+        int setHealth(healthVar)
+        {
+            health = healthVar;
+        }
+        int setmageLevel(magic)
+        {
+            magicLevel = magic;
+        }
+        int setmovement(moveSpeed)
+        {
+            speed = moveSpeed;
+        }
+        int setAttack(attVar)
+        {
+            attack = attVar;
+        }
+        int setExperience(exp)
+        {
+            experience = exp;
+        }
+        int setCash(wallet)
+        {
+            cash = wallet;
+        }
+
+};
+
+
+#endif // PLAYER_H
