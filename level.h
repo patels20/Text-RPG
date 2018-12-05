a821a0530df7c23ba808bc38632ea3782c168392
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 15:40:13 -0500

    Level branch

---
+++ level.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <random>
+
+class level{
+    private:
+        int level;
+        string endDestination;
+        int numMonsters;
+    public:
+        int getLevel(lvl)
+        {
+            level = lvl;
+        }
+        string endDest(dest)
+        {
+            endDestination = dest;
+        }
+        int monsters(most)
+        {
+            numMonsters = most;
+        }
+
+
+
+
+
+
+};
