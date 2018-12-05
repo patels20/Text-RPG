7ca344e28c1b63f562e13e10707f5f3f64db32de
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 16:51:49 -0500

    Update level.cpp

--- level.cpp
+++ level.cpp
@@ -5,7 +5,7 @@
 #include <random>
 
 int randomFight = (rand() %50) +20;
-
+int userfightInt = 0;
 void level1(string userInt, monster& mon,)
 {
     cout << "------------------------------ " << endl;
@@ -19,12 +19,26 @@ void level1(string userInt, monster& mon,)
         case "Right":
         case "right":
             cout << "You've initiated to fight" << monster.getName() << endl;
-            
+            do
+            {
+                cout << "\t\tYou're Turn" << endl;
+                cout << "------------------------------ " << endl;
+                cout << "\t\t1. Attack \t2.Dodge" << endl;
+                cout << "\t\t\t3.Heal" << endl;
+                cout << "------------------------------ " << endl;
+                cin >> userfightInt;
+                switch(userfightInt)
+                {
+                    case 1:
+                        
+                }
+                
+            }
+            while(monster.health != 0; || player.health != 0)            
         case "Left":
         case "left":
             cout << "You've Encountered " << monster.getName() << endl;
             cout << ""
-
 
     }
 
