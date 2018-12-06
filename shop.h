eb6aa987e36164aad3109f1edabb95c50e2bbc55
WillKelsey <will.kelsey@cox.net>
2018-12-06 18:10:04 -0500

    Shop Complete

--- shop.h
+++ shop.h
@@ -12,24 +12,49 @@ void shop(player player1)
 		cout << "\t\t\t\t\t\t\tShop" << endl;
 		cout << "------------------------------------------------------------------------------------------------------------------------\n" << endl;
 		cout << "Availible Items:" << endl;
-		cout << "(G) Gas: 10BTC \t (H) Health: 10BTC \t (D) Instant Damage: 20BTC \t (R) RED BULL: 1,000 BTC" << endl;
+		cout << "(G) Gas: 10BTC \t (H) Health: 10BTC \t (R) RED BULL: 1,000 BTC" << endl;
 
 		cin << userBuy;
 		system("cls");
 		
 		switch(userBuy)
 		{
-			case "G"
+			case "G":
+			case "g":
 			if(player1.cash >= 10)
 			{
-				player.inventory 
+				player1.gas++;
+				player1.cash -= 10;
+			}
+			else
+			{
+				cout << "Not enough money..." << endl;
+			}
+			break;
+			
+			case "H":
+			case "h":
+			if(player1.cash >= 10)
+			{
+				player1.healthpack++;
+				player1.cash -= 10;
 			}
 			else
 			{
 				cout << "Not enough money..." << endl;
 			}
 			
-			
+			case "R":
+			case "r":
+			if(player1.cash >= 1000)
+			{
+				player1.redbull++;
+				player1.cash -= 1000;
+			}
+			else
+			{
+				cout << "Not enough money..." << endl;
+			}
 		}
 	}
 }
