5d27f58d66fd2d237d1ec0d9fa32abd7ee297679
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-06 01:40:54 -0500

    Update fight.h

--- fight.h
+++ fight.h
@@ -2,11 +2,7 @@
 #include "monster.h"
 #include "level.h"
 
-
 using namespace std;
-
-
-
 
 int userfightInt = 0;
 
@@ -16,10 +12,10 @@ void fight(player player1, monster mon)
     bool won = false;
     int playerAttack1 = player1.getAttack() - mon.getDefense();
     int monsterAttack1 = mon.getAttack() - player1.getDefense();
+    int opac = 100 - player1.getDefense();
 
     do
     {
-
         do
             {
                 int random = rand() % 100;
@@ -39,28 +35,52 @@ void fight(player player1, monster mon)
                     switch(userfightInt)
                     {
                         case 1:
-
-
                             if(random <=50)
                             {
                             system("cls");
                             cout << "You've landed a successful attack" << endl;
                             cout << "Damage Dealt : " << playerAttack1 << endl;
                             mon.setHealth(mon.getHealth()-playerAttack1);
+                                if(random2 <= 90)
+                                {
+                                    cout <<mon.getName() << " has successfully landed a hit." << endl;
+                                    cout << "You've taken " << monsterAttack1 << endl;
+                                    player1.setHealth((player1.getHealth()-monsterAttack1));
+                                }
+                                else
+                                {
+                                    cout << "Monster has missed an attack" << endl;
+                                }
                             }
                             else if(random >50)
                             {
                                 system("cls");
                                 cout << "You've missed the attack" << endl;
+                                 if(random2 <= 25)
+                                {
+                                    cout <<mon.getName() << " has successfully landed a hit." << endl;
+                                    cout << "You've taken " << monsterAttack1 << endl;
+                                    player1.setHealth((player1.getHealth()-monsterAttack1));
+                                }
+                                else
+                                {
+                                    cout << "Monster has missed an attack" << endl;
+                                }
                             }
-                           /* if(random2 <= 90)
-                            {
-                                system("cls");
-                                player1.setHealth(monsterAttack1);
-                            }*/
+
+
+                       /* if(random2 <= 90)
+                        {
+                            system("cls");
+                            player1.setHealth(monsterAttack1);
+                        }*/
                             break;
                         case 2:
                             cout << "You've Dodge the attack" << endl;
+                            if(random2 <= 101)
+                            {
+                                cout << "Monster has missed his/her attack" << endl;
+                            }
                             break;
                         case 3:
                             if((player1.health + 10) >= 100)
@@ -77,8 +97,11 @@ void fight(player player1, monster mon)
                         default:
                             cout << "Invalid Fight Option" << endl;
                             break;
-                        }
+                    }
+
+
             }
+
             while(!(mon.health <= 0 || player1.health <= 0));   // 33 <= 0 or 44 <= 0
             if(mon.health <= 0)
             {
