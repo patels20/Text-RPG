2a321a579daee0dfde21d15240f2ac3563cde1fc
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-06 16:54:58 -0500

    Update fight.h

--- fight.h
+++ fight.h
@@ -1,6 +1,5 @@
 #include "player.h"
 #include "monster.h"
-#include "level.h"
 
 using namespace std;
 
@@ -22,12 +21,14 @@ void fight(player player1, monster mon)
                 int random2 = rand() % 100;
 
                 cout << "------------------------------------------------------------------------------------------------------------------------\n" << endl;
-                cout << "\t\t\t\t\t\t\tYour Health : " << player1.health << endl;
-                cout << "\t\t\t\t\t\t\tMonster Health : " << mon.health <<  "\n" << endl;
+                cout << "\t\t\t"<<player1.getName()<<" Status \t\t\t\t "<<mon.getName()<<" Status" << endl;
+                cout << "\t\t\tHealth :" << player1.getHealth() << "\t\t\t\t Health : " << mon.getHealth() << endl;
+                cout << "\t\t\tAttack :" << player1.getAttack() << "\t\t\t\t Attack : " << mon.getAttack() << endl;
+                cout << "\t\t\tDefense :" << player1.getDefense() << "\t\t\t\t Defense : " << mon.getDefense() << endl;
                 cout << "------------------------------------------------------------------------------------------------------------------------\n" << endl;
                 cout << "\t\t\t\t\t\t\tYou're Turn" << endl;
                 cout << "------------------------------------------------------------------------------------------------------------------------\n" << endl;
-                cout << "\t\t\t\t\t\t1. Attack \t2.Dodge\t\t3.Heal" << endl;
+                cout << "\t\t\t\t\t1. Attack \t\t2.Dodge\t\t3.Heal\t\t4.View Items"<< endl;
                 cout << "------------------------------------------------------------------------------------------------------------------------\n" << endl;
                 cin >> userfightInt;
                 system("cls");
@@ -43,7 +44,7 @@ void fight(player player1, monster mon)
                             mon.setHealth(mon.getHealth()-playerAttack1);
                             system("pause");
                             system("cls");
-                                if(random2 <= 90)
+                                if(random2 <= 35)
                                 {
                                     cout <<mon.getName() << " has successfully landed a hit." << endl;
                                     cout << "You've taken " << monsterAttack1 << endl;
@@ -62,7 +63,7 @@ void fight(player player1, monster mon)
                                 cout << "You've missed the attack" << endl;
                                 system("pause");
                                 system("cls");
-                                 if(random2 <= 90)
+                                 if(random2 <= 35)
                                 {
                                     cout <<mon.getName() << " has successfully landed a hit." << endl;
                                     cout << "You've taken " << monsterAttack1 << endl;
@@ -106,6 +107,9 @@ void fight(player player1, monster mon)
                             system("pause");
                             system("cls");
                             break;
+                        case 4:
+                            system("cls");
+                            break;
                         default:
                             cout << "Invalid Fight Option" << endl;
                             system("pause");
@@ -117,7 +121,6 @@ void fight(player player1, monster mon)
             while(!(mon.health <= 0 || player1.health <= 0));   // 33 <= 0 or 44 <= 0
             if(mon.health <= 0)
             {
-                player1.curLevel+=1;
                 won = true;
                 player1.progression+=1;
             }
