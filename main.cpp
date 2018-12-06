102b9bca58640f1da19f7b5dc7483f7da61ceb2f
akshar008 <36671757+akshar008@users.noreply.github.com>
2018-12-06 01:55:48 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "level.cpp"
-#include "wizard.h"
 #include "player.h"
 #include "monster.h"
 #include "level.h"
@@ -14,24 +13,32 @@ int main()
 {
     int userInput = 0;
     cout << "Please pick one of the characters below:";
-    cout << "\n\n(1)\nWizard\nWeapon: Staff\n";
-    cout << "\n\n(2)\nArcher\nWeapon: Bow and Arrow\n";
-    cout << "\n\n(3)\nWarrior\nWeapon: Shield and Sword\n";
-	//Create Wizard = myWizard wizard("God",100,20,0,0,100);
-	cin >> userInput;
-	for(int i = 1; i <= 5; i++){
-	switch(userInput){
+    cout << "\n\n(1)\nWIZARD\nWeapon: Staff \nHealth: 100 \nAttack: 85 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 90\n";
+    cout << "\n\n(2)\nARCHER\nWeapon: Bow and Arrow\nHealth: 100 \nAttack: 80 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 75\n";
+    cout << "\n\n(3)\nWARRIOR\nWeapon: Shield and Sword\nHealth: 100 \nAttack: 100 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 90\n";
+    //Create Wizard = myWizard wizard("God",100,20,0,0,100);
+    cin >> userInput;
+    for(int i = 1; i <= 5; i++){
+    switch(userInput){
         case 1:
-            player player1("God",100,100,30,20,0,0,1);
+            player player1("God",100,100,30,10,0,0);
             //health,energy,attack,defense -Player
             //health,attack,defense - Monster
-            if(player1.progression == 1)
+            if(player1.progression = 1)
             {
                 level1(player1,monster("Shia",100,20,12));
             }
             if(player1.progression = 2)
             {
                 level2(player1,monster("Shia",100,20,12));
+            }
+             if(player1.progression = 3)
+            {
+                level3(player1,monster("Shia",100,20,12));
+            }
+             if(player1.progression = 4)
+            {
+                level4(player1,monster("Shia",100,20,12));
             }
             break;
       /*  case 2:
@@ -44,7 +51,8 @@ int main()
         default:
             cout << " ok" << endl;
             */
-	}
-	}
+    }
+    }
     return 0;
+
 }
