c50605f17177098f4d9b8ccd16cb4769e9321782
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-05 19:13:32 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
-#include <player.h>
-#include <wizard.h>
+#include "level.cpp"
+#include "wizard.h"
+#include "player.h"
+#include "monster.h"
+#include "level.h"
+#include <iostream>
+#include <string>
+#include <random>
 
+
+using namespace std;
 
 int main()
 {
+    int userInput = 0;
     cout << "Please pick one of the characters below:";
     cout << "\n\n(1)\nWizard\nWeapon: Staff\n";
     cout << "\n\n(2)\nArcher\nWeapon: Bow and Arrow\n";
     cout << "\n\n(3)\nWarrior\nWeapon: Shield and Sword\n";
-	
 	//Create Wizard = myWizard wizard("God",100,20,0,0,100);
-    return 0;
-    if(person > 3)
-		{
-			system("cls");
-			cout << "You must enter an integer between 1 and 3!!!!\n\n";
-			system("pause");
-			system("cls");
-		}
+	cin >> userInput;
 
-    
-    
+	switch(userInput){
+        case 1:
+            player("God",100,20,0,0,100);
+            monster("Shia",100,20,1);
+            level1(player("God",100,20,0,0,100),monster("Shia",100,20,1));
+            break;
+      /*  case 2:
+            cout << "Nothing" << endl;
+            break;
+        case 3:
+            cout << "Warrior" << endl;
+            break;
+
+        default:
+            cout << " ok" << endl;
+            */
+	}
+    return 0;
 
 }
