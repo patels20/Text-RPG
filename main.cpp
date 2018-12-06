a641db0eb27fd777acdaee873c6f031216e324e8
Shail  Patel <44775847+patels20@users.noreply.github.com>
2018-12-06 13:40:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,53 +6,156 @@
 #include <string>
 #include <random>
 
-
 using namespace std;
+
 
 int main()
 {
-    int userInput = 0;
-    cout << "Please pick one of the characters below:";
-    cout << "\n\n(1)\nWIZARD\nWeapon: Staff \nHealth: 100 \nAttack: 85 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 90\n";
-    cout << "\n\n(2)\nARCHER\nWeapon: Bow and Arrow\nHealth: 100 \nAttack: 80 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 75\n";
-    cout << "\n\n(3)\nWARRIOR\nWeapon: Shield and Sword\nHealth: 100 \nAttack: 100 \nExp: 0 \nCash: 0 \nEnergy: 100 \nDefense: 90\n";
-    //Create Wizard = myWizard wizard("God",100,20,0,0,100);
-    cin >> userInput;
-    for(int i = 1; i <= 5; i++){
-    switch(userInput){
-        case 1:
-            player player1("God",100,100,30,10,0,0);
-            //health,energy,attack,defense -Player
-            //health,attack,defense - Monster
-            if(player1.progression = 1)
+    string gameStarter = "";
+    char game = 'S';
+    cout <<"Enter you're name : " << endl;
+    cin >> gameStarter;
+    system("cls");
+    cout << "------------------------------------------------------------------------------------------------------------------------ " << endl;
+    cout <<"\t\t\t\t\t\tWelcome to the Adventures of " << gameStarter << endl;
+    cout << "------------------------------------------------------------------------------------------------------------------------ " << endl;
+    cout <<"S : Start Game \t\t C: Credits " << endl;
+    cout <<"Q : Quit " << endl;
+    cin >> game;
+    switch(game)
+    {
+        case 'C':
+        case 'c':
+            system("cls");
+            cout <<"Dungeon Adventures : Text Mania " << endl;
+            Sleep(900);
+            cout <<"Written By : Shail Patel , William Kelsey , Akshar Patel " << endl;
+            Sleep(900);
+            cout <<"Software Used : Code Blocks " << endl;
+            Sleep(900);
+            cout <<"Language : C++"<<endl;
+            Sleep(900);
+            cout <<"Published December 7th 2018" <<endl;
+        break;
+        case 'Q':
+        case 'q':
+            system("cls");
+            cout <<"You're Leaving Dungeon Adventures " << endl;
+            Sleep(900);
+            system("cls");
+            cout <<"Goodbye " << endl;
+        break;
+        case 'S':
+        case 's':
+            system("cls");
+            int userInput = 0;
+            cout << "Please pick one of the characters below:";
+            cout << "\n\nWizard: Weapon: Staff" << endl;
+            cout << "    \tHealth:      100"<<endl;
+            cout << "       \tEnergy:      100"<<endl;
+            cout << "   \tAttack:      1000"<<endl;
+            cout << "       \tDefense:     10"<<endl;
+            cout << "    \tCash:        0"<<endl;
+            cout << "    \tExperience:  0   "<<endl;
+            cout << "\n\nArcher: Weapon: Bow and Arrow\n";
+            cout << "    \tHealth:      100"<<endl;
+            cout << "       \tEnergy:      100"<<endl;
+            cout << "   \tAttack:      1000"<<endl;
+            cout << "       \tDefense:     10"<<endl;
+            cout << "    \tCash:        0"<<endl;
+            cout << "    \tExperience:  0   "<<endl;
+            cout << "\n\nWarrior:Weapon: Shield and Sword\n";
+            cout << "    \tHealth:      100"<<endl;
+            cout << "       \tEnergy:      100"<<endl;
+            cout << "   \tAttack:      1000"<<endl;
+            cout << "       \tDefense:     10"<<endl;
+            cout << "    \tCash:        0"<<endl;
+            cout << "    \tExperience:  0   "<<endl;
+            Sleep(1000);
+            cout <<"\nDecide :"<<endl;
+            Sleep(900);
+            cout <<"1: Wizard" << endl;
+            Sleep(900);
+            cout << "2: Archer"<< endl;
+            Sleep(900);
+            cout <<"3: Warrior" << endl;
+            Sleep(900);
+            cin >> userInput;
+            for(int i = 1; i <= 5; i++){
+            switch(userInput)
             {
-                level1(player1,monster("Shia",100,20,12));
-            }
-            if(player1.progression = 2)
-            {
-                level2(player1,monster("Shia",100,20,12));
-            }
-             if(player1.progression = 3)
-            {
-                level3(player1,monster("Shia",100,20,12));
-            }
-             if(player1.progression = 4)
-            {
-                level4(player1,monster("Shia",100,20,12));
-            }
-            break;
-      /*  case 2:
-            cout << "Nothing" << endl;
-            break;
-        case 3:
-            cout << "Warrior" << endl;
-            break;
+                case 1:
+                    {
+                        player player1("God",100,100,1000,10,0,0);
+                    //health,energy,attack,defense -Player
+                    //health,attack,defense - Monster
+                    if(player1.progression = 1)
+                    {
+                        level1(player1,monster("Shia",100,20,12));
+                    }
+                    if(player1.progression = 2)
+                    {
+                        level2(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 3)
+                    {
+                        level3(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 4)
+                    {
+                        level4(player1,monster("Shia",100,20,12));
+                    }
+                    break;
+                    }
+                case 2:
+                    {
+                    player player1("God",100,100,1000,10,0,0);
+                    if(player1.progression = 1)
+                    {
+                        level1(player1,monster("Shia",100,20,12));
+                    }
+                    if(player1.progression = 2)
+                    {
+                        level2(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 3)
+                    {
+                        level3(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 4)
+                    {
+                        level4(player1,monster("Shia",100,20,12));
+                    }
+                    break;
+                    }
+                case 3:
+                    {
+                        player player1("God",100,100,1000,10,0,0);
 
-        default:
-            cout << " ok" << endl;
-            */
-    }
+                    if(player1.progression = 1)
+                    {
+                        level1(player1,monster("Shia",100,20,12));
+                    }
+                    if(player1.progression = 2)
+                    {
+                        level2(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 3)
+                    {
+                        level3(player1,monster("Shia",100,20,12));
+                    }
+                     if(player1.progression = 4)
+                    {
+                        level4(player1,monster("Shia",100,20,12));
+                    }
+                    break;
+                    }
+                default:
+                    cout << " ok" << endl;
+
+            }
+            }
+        break;
     }
     return 0;
-
 }
